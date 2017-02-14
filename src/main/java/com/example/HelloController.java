8d4518b189883fae40dc6525a76944c1043cc36a
eparkhurst <eli.parkhurst@gmail.com>
2017-02-14 13:25:39 -0700

    cleaned up imports in Hellocontroller

--- src/main/java/com/example/HelloController.java
+++ src/main/java/com/example/HelloController.java
@@ -2,7 +2,6 @@ package com.example;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 public class HelloController {
